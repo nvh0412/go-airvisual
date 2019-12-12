35c8d04db9a9f0c4200b22ae7c6bfa9a4e565297
Hoa Nguyen <hoa.nguyen@employmenthero.com>
2019-12-12 09:40:03 +0700

    Add country api structure

--- airvisual/airvisual.go
+++ airvisual/airvisual.go
@@ -1,12 +1,16 @@
 package airvisual
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 	"net/url"
 )
 
 const (
-	defaultBaseURL = "api.airvisual.com"
+	defaultBaseURL = "https://api.airvisual.com/v2"
 )
 
 type Client struct {
@@ -21,9 +25,50 @@ type service struct {
 	client *Client
 }
 
-type Response struct {
-	*http.Response
+func (c *Client) NewRequest(method, urlStr string) (*http.Request, error) {
+	u, err := c.BaseURL.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *Client) Do(ctx context.Context, req *http.Request, v interface{}) (*http.Response, error) {
+	if ctx == nil {
+		return nil, errors.New("context must be non-nil")
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
+	}
+
+	defer resp.Body.Close()
+
+	if v != nil {
+		decErr := json.NewDecoder(resp.Body).Decode(v)
+		if decErr == io.EOF {
+			decErr = nil
+		}
+		if decErr != nil {
+			err = decErr
+		}
+	}
+
+	return resp, err
 }
 
 type ErrorResponse struct {
+	Response *http.Response
+	Message  string `json:"message"` // error message
 }
--- airvisual/countries.go
+++ airvisual/countries.go
@@ -1,11 +1,28 @@
 package airvisual
 
+import (
+	"context"
+	"net/http"
+)
+
 type CountryService service
 
 type Country struct {
 	Name string `json:"country"`
 }
 
-func (s *CountryService) ListCountries() ([]Country, *Response, error) {
-	return nil, nil, nil
+func (s *CountryService) ListCountries(ctx context.Context) (*[]Country, *http.Response, error) {
+	req, err := s.client.NewRequest("GET", "countries")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	countries := &[]Country{}
+
+	resp, err := s.client.Do(ctx, req, countries)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return countries, resp, nil
 }
